c5953625f708d9bef72eb638596f91a750d292ec
Roberto Alejandro Salazar Fierros <20000800@alumnos.utleon.edu.mx>
2023-02-24 22:41:24 -0600

    Ejercicio 1 realizado 24/02/2023

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from collections import Counter
 import forms
 
 app = Flask(__name__)
@@ -21,8 +22,34 @@ def cajasDinamicas():
 
 @app.route("/cajas_renderizadas", methods=["GET", "POST"])
 def cajasRenderizadas():
-    num = request.form.get("txtNum")
-    return render_template("cajas_renderizadas.html", num = num)
+    numCajas = request.form.get("txtNumCajas")
+    numCajas = int(numCajas)
+    return render_template("cajas_renderizadas.html", numCajas = numCajas)
+
+@app.route("/resultado_cajas", methods=["GET", "POST"])
+def resultadoCajas():
+    maximo = 0
+    minimo = 0
+    promedio = 0
+    lista = []
+    duplicados = {}
+    contador = 1
+    totalCajas = request.form.get("txtTotalCajas")
+    totalCajas = int(totalCajas)
+    for num in range(totalCajas):
+        lista.append(request.form.get("txtN{}".format(contador)))
+        contador += 1
+    maximo = max(lista)
+    minimo = min(lista)
+    for num in lista:
+        promedio = promedio + int(num)
+    promedio = promedio//len(lista)
+    conteo=Counter(lista)
+    for clave in conteo:  
+        valor=conteo[clave]
+        if valor != 1:
+            duplicados[clave] = valor
+    return render_template("resultado_cajas.html", lista = lista, maximo = maximo, minimo = minimo, promedio = promedio, duplicados = duplicados)
 
 if __name__ == "__main__":
     app.run(debug = True)
